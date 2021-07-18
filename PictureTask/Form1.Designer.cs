db249e1dc07e3caac38e9dae266cd103998830e8
adekeagle <2726949-adekeagle@users.noreply.gitlab.com>
2021-07-18 18:41:54 +0200

    Initial commit

---
+++ PictureTask/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace PictureTask
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.pbPicture = new System.Windows.Forms.PictureBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(408, 29);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(96, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Dodaj zdjęcie";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pbPicture
+            // 
+            this.pbPicture.Location = new System.Drawing.Point(33, 29);
+            this.pbPicture.Name = "pbPicture";
+            this.pbPicture.Size = new System.Drawing.Size(335, 229);
+            this.pbPicture.TabIndex = 1;
+            this.pbPicture.TabStop = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(408, 67);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(96, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Usuń zdjęcie";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(408, 235);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(96, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Zamknij";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 286);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.pbPicture);
+            this.Controls.Add(this.btnAdd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Show pictures";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.PictureBox pbPicture;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ PictureTask/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PictureTask
+{
+    public partial class MainForm : Form
+    {
+        private OpenFileDialog fileDialog;
+        private Image picture;
+        private const string FILENAME = "last_picture.txt";
+        public MainForm()
+        {
+            InitializeComponent();
+
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + FILENAME) && !string.IsNullOrEmpty(readFilePath()))
+            {
+                picture = Image.FromFile(readFilePath());
+                pbPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+                pbPicture.Image = picture;
+            }
+
+            if (!isPicture())
+            {
+                btnDelete.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "JPG Files | *.jpg";
+
+            if (!isPicture())
+            {
+                btnDelete.Enabled = false;
+            }
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                picture = Image.FromFile(fileDialog.FileName);
+                pbPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+                pbPicture.Image = picture;
+                saveFilePath(fileDialog.FileName);
+                btnDelete.Enabled = true;
+            }
+        }
+
+        private static void saveFilePath(string path)
+        {
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + FILENAME, path);
+        }
+
+        private static string readFilePath()
+        {
+            return File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + FILENAME);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            pbPicture.Image.Dispose();
+            pbPicture.Image = null;
+            saveFilePath("");
+            btnDelete.Enabled = false;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private bool isPicture()
+        {
+            if (pbPicture.Image == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
